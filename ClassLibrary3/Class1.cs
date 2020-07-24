ed36d4b167ab8c1a4e4fe73d6dfca5d4764913fe
Andrei Epure <andrei.epure@sonarsource.com>
2020-07-24 17:15:03 +0200

    simple netcore3 app

---
+++ ClassLibrary3/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary3
+{
+    public class Class1
+    {
+        public string S = "S";
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("S", new Class1().S);
+        }
+    }
+}
